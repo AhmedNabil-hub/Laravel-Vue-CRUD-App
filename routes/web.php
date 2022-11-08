facc48f4b396ba92711662d81f5e104cae58bc51
Ahmed Nabil <a7med.nabil1655@gmail.com>
2022-11-08 15:38:02 +0200

    init project

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Auth::routes();
+
+Route::get('/', function () {
+	return view('welcome');
+});
+
+Route::get('/home', [HomeController::class, 'index'])
+	->name('home');
+
+Route::get('/about', function() {
+	return view('example');
+});
